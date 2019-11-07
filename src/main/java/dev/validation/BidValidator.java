19d87d6fcfe6fc43495c58e7353ad996b88d0908
Karinka <kharinakaryna@gmail.com>
2019-11-07 19:10:17 +0200

    created class for data validation of Bid

---
+++ src/main/java/dev/validation/BidValidator.java
@@ -0,0 +1,41 @@
+package dev.validation;
+
+import dev.entities.Bid;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class BidValidator {
+
+    private boolean isValidDateTime(LocalDateTime dateTime) {
+
+        LocalDateTime nowDateTime = LocalDateTime.now();
+
+        int year = dateTime.getYear();
+        int month = dateTime.getMonthValue();
+        int day = dateTime.getDayOfMonth();
+        int hour = dateTime.getHour();
+        int minute = dateTime.getMinute();
+
+        boolean isValidYear = (year >= nowDateTime.getYear());
+        boolean isValidMonth = (month >= nowDateTime.getMonthValue());
+        boolean isValidDay = (day >= nowDateTime.getDayOfMonth());
+        boolean isValidHour = (hour >= nowDateTime.getHour());
+        boolean isValidMinute = (minute >= nowDateTime.getMinute());
+
+        return isValidYear
+                && isValidMonth
+                && isValidDay
+                && isValidHour
+                && isValidMinute;
+    }
+
+    private boolean isValidRouteNumber(String routNumber) {
+        return routNumber.length() == 8;
+    }
+
+    public boolean isValid(Bid bid) {
+        return isValidDateTime(bid.getDateTime()) && isValidRouteNumber(bid.getRouteNumber());
+    }
+}
